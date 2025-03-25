542815a07d18242f4646fd224e7679ceba471c8b
Lawliet26 <luisacoy2005@gmail.com>
2025-03-24 22:37:43 -0500

    Creacion de menu lateral de filtros

---
+++ proyectoExplore/js/filtros.js
@@ -0,0 +1,33 @@
+const icono = document.getElementById("icono");
+const barraLateral = document.querySelector(".barra-lateral");
+const span = document.querySelector("span");
+const labels = document.querySelectorAll("label");
+const selects = document.querySelectorAll("select");
+const boton = document.getElementById("envio");
+const menu =document.querySelector(".menu-lateral");
+
+menu.addEventListener("click",()=>{
+    barraLateral.classList.toggle("max-barra-lateral");
+    if(barraLateral.classList.contains("max-barra-lateral")){
+        menu[0].style.display = "none";
+        menu[1].style.display = "block";
+    }
+    else{
+        menu[0].style.display = "block";
+        menu[1].style.display = "none";
+    }
+});
+
+icono.addEventListener("click", () => {
+  barraLateral.classList.toggle("mini-barra-lateral");
+  span.classList.toggle("oculto");
+  boton.classList.toggle("oculto-boton");
+
+  labels.forEach(label=>{
+    label.classList.toggle("oculto");
+  });
+
+  selects.forEach(select =>{
+    select.classList.toggle("oculto");
+  });
+});

ab4e9cc59fa1956ee033b7cd5888e312e76f9225
Lawliet26 <luisacoy2005@gmail.com>
2025-03-25 20:57:09 -0500

    Creacion de slider de paquetes

--- proyectoExplore/js/filtros.js
+++ proyectoExplore/js/filtros.js
@@ -4,18 +4,17 @@ const span = document.querySelector("span");
 const labels = document.querySelectorAll("label");
 const selects = document.querySelectorAll("select");
 const boton = document.getElementById("envio");
-const menu =document.querySelector(".menu-lateral");
+const menu = document.querySelector(".menu-lateral");
 
-menu.addEventListener("click",()=>{
-    barraLateral.classList.toggle("max-barra-lateral");
-    if(barraLateral.classList.contains("max-barra-lateral")){
-        menu[0].style.display = "none";
-        menu[1].style.display = "block";
-    }
-    else{
-        menu[0].style.display = "block";
-        menu[1].style.display = "none";
-    }
+menu.addEventListener("click", () => {
+  barraLateral.classList.toggle("max-barra-lateral");
+  if (barraLateral.classList.contains("max-barra-lateral")) {
+    menu[0].style.display = "none";
+    menu[1].style.display = "block";
+  } else {
+    menu[0].style.display = "block";
+    menu[1].style.display = "none";
+  }
 });
 
 icono.addEventListener("click", () => {
@@ -23,11 +22,29 @@ icono.addEventListener("click", () => {
   span.classList.toggle("oculto");
   boton.classList.toggle("oculto-boton");
 
-  labels.forEach(label=>{
+  labels.forEach((label) => {
     label.classList.toggle("oculto");
   });
 
-  selects.forEach(select =>{
+  selects.forEach((select) => {
     select.classList.toggle("oculto");
+  });
+});
+
+// slider de tarjetas
+const productContainer = [...document.querySelectorAll(".contenedor")];
+const nextBtn = [...document.querySelectorAll(".next-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainer.forEach((item, i) => {
+  let dimensiones = item.getBoundingClientRect();
+  let cWidth = dimensiones.width;
+
+  nextBtn[i].addEventListener("click", () => {
+    item.scrollLeft += cWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= cWidth;
   });
 });
